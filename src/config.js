124514573a75e1e6695d75e3e6eb292308a6f2da
Karla <indiablonix@gmail.com>
2024-08-16 03:36:02 -0700

    Add config file

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Karla Walker - Front-End Developer and Designer";
+export const SITE_DESCRIPTION = "Karla Walker's personal site.";
+export const SITE_OWNER = "Karla Walker";
+export const OWNER_DESCRIPTION = "Front-End Developer and Designer";
